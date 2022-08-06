61c5fa8a7d03196ef9d6e8e1f50f9a824464b74c
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-06 11:12:08 +0630

    Custom Log Middleware

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-
+const {logger} = require('./middleware/logger')
 const PORT = process.env.PORT || 3500
+
+app.use(logger)
+
+app.use(express.json())
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
