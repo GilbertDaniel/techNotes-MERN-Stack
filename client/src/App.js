229a2ca37c9a2604c0f837570e6285b40bfd322c
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-13 12:55:40 +0630

    Implementing Users and Notes List

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Login from './features/auth/Login';
 import Public from './components/Public';
 import DashLayout from './components/DashLayout';
 import Welcome from './features/auth/Welcome';
+import NotesList from './features/users/NotesList';
+import UsersList from './features/notes/UsersList';
 
 function App() {
   return (
@@ -11,9 +13,18 @@ function App() {
       <Route path='/' element={<Layout/>}>
           <Route index element={<Public/>}/>
           <Route path="login" element={<Login/>}/>
+          
+          {/* Dash Start */}
           <Route path='dash' element={<DashLayout/>}>
               <Route index element={<Welcome/>}/>
+              <Route path='notes'>
+                <Route index element={<NotesList/>}/>
+              </Route>
+              <Route path='users'>
+                <Route index element={<UsersList/>}/>
+              </Route>
           </Route>
+          {/* Dash End */}
       </Route>
     </Routes>
   );
